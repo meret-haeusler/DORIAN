5a1a63b8b9ac2724b8df49bc6e81e76943fa882e
meret <meret.haeusler@uni-tuebingen.de>
2024-08-14 10:11:30 +0200

    [UPDATE] Implement LogWriter for all correction modes

--- src/main/java/utils/LogWriter.java
+++ src/main/java/utils/LogWriter.java
@@ -1,37 +1,64 @@
 package utils;
 
 import datastructure.CorrectionMode;
+import datastructure.Fasta;
+import htsjdk.samtools.util.SamLocusIterator;
 
 import java.text.DecimalFormat;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-import static dorian.BaseCalling.MAX_FREQ;
 import static dorian.dorian.*;
-import static dorian.dorian.base_call;
 
 public class LogWriter {
 
+    /**
+     * Adds a line to the log file documenting the determination of a base call
+     * @param refInfo   Information on the current reference position
+     * @param ref       Reference as fasta object
+     * @param cov       Observed read coverage at the position
+     * @param cnts      Base counts before correction
+     * @param cntsCor   Base counts after correction
+     * @param call      Final base call
+     * @param callFreq  Frequency of final base call (-1 if call is 'N')
+     */
+    public static void addLog(SamLocusIterator.LocusInfo refInfo, Fasta ref, int cov, Map<Character, Double> cnts,
+                              Map<Character, Double> cntsCor, Character call, Double callFreq) {
 
-    static DecimalFormat df = new DecimalFormat("#.##");
+        // Get reference infos
+        String chrom = refInfo.getSequenceName();
+        int refPos = refInfo.getPosition();
+        char refBase = ref.getSequence().charAt(refPos-1);
 
-    public static void addLog(String message) {
+        // Add to log file
+        // Uncorrected: CHROM POS REF COV ALLELE_COUNTS BASE_CALL BASE_FREQ
+        // Corrected:   CHROM POS REF COV ALLELE_COUNTS_PRIOR ALLELE_COUNTS_CORRECTED BASE_CALL BASE_FREQ
+        if (cor_mode.equals(CorrectionMode.NO_COR)) {
+            file_logger.info("{}\t{}\t{}\t{}\t{}\t{}\t{}",
+                    chrom, refPos, refBase, cov, MapToString(cnts), call, callFreq);
+        } else {
+            // Log file
+            file_logger.info("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}",
+                    chrom, refPos, refBase, cov, MapToString(cnts), MapToString(cntsCor), call, callFreq);
 
-        // Add info to logger if reads were corrected
-        if (variant_type.isVariant()) {
-            // Get base call frequency
-            double call_freq = (base_call.equals('N')) ? -1.0 : MAX_FREQ;
-
-            file_logger.info(contig + "\t" + ref_pos + "\t" + ref.getSequence().charAt(ref_pos-1) + "\t"
-                    + mapping_reads.size() + "\t" + MapToString(base_counts_uncor) + "\t" + MapToString(base_counts) + "\t"
-                    + base_call + "\t" + df.format(call_freq));
-
-            int roi_start = Math.max(ref_pos - 3, 0);
-            int roi_end = Math.min(ref_pos + 2, ref.getSequence().length());
-            roi_tab.info(contig + "\t" + roi_start + "\t" + roi_end + "\t" + "CORRECTED_POS:" + ref_pos);
+            // ROI file
+            int roi_start = Math.max(refPos - 3, 0);
+            int roi_end = Math.min(refPos + 2, ref.getSequence().length());
+            roi_tab.info("{}\t{}\t{}\tCORRECTED_POS:{}", chrom, roi_start, roi_end, refPos);
         }
+    }
 
-        //TODO: Remove when no longer needed
-        if (cor_mode.equals(CorrectionMode.NO_COR)){
-            file_logger.info(ref_pos + "\t" + base_call + "\t" + df.format(MAX_FREQ));
-        }
+
+    /**
+     * Converts HashMap entries to a formatted string
+     *
+     * @param map Map of allele counts
+     * @return String of allele counts
+     */
+    private static String MapToString(Map<Character, Double> map) {
+        DecimalFormat df = new DecimalFormat("#.##");
+        return map.keySet().stream()
+                .map(key -> key + "=" + df.format(map.get(key)))
+                .collect(Collectors.joining(","));
     }
 }
