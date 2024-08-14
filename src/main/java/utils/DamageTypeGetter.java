c5ae90d5f6d349d2e3b4bfa43d3d3f87639483de
meret <meret.haeusler@uni-tuebingen.de>
2024-08-14 14:11:51 +0200

    [INIT] Add class to determine damage type of given mapping position

---
+++ src/main/java/utils/DamageTypeGetter.java
@@ -0,0 +1,67 @@
+package utils;
+
+import datastructure.DamageType;
+import datastructure.MappingPosition;
+
+import java.util.List;
+
+public class DamageTypeGetter {
+
+    public static DamageType getDamageTypeRefbased(List<MappingPosition> mappingReads, char refBase) {
+        boolean T_forward = false;
+        boolean A_reverse = false;
+
+        // Check which bases are present in the reads
+        for (MappingPosition mp : mappingReads) {
+            if (!mp.is_reverse && mp.base == 'T') {
+                T_forward = true;
+            } else if (mp.is_reverse && mp.base == 'A') {
+                A_reverse = true;
+            }
+        }
+
+        // Determine damage type
+        if (refBase == 'C' && T_forward) {
+            return DamageType.CT;
+        } else if (refBase == 'G' && A_reverse) {
+            return DamageType.GA;
+        } else {
+            return DamageType.NONE;
+        }
+    }
+
+    /**
+     * Determines if a position is damaged and which damage pattern is present
+     *
+     * @param mappingReads Mapping reads
+     * @return DamageType
+     */
+    public static DamageType getDamageTypeReffree(List<MappingPosition> mappingReads){
+        boolean C = false;
+        boolean T_forward = false;
+        boolean G = false;
+        boolean A_reverse = false;
+
+        // Check which bases are present in the reads
+        for (MappingPosition mp : mappingReads) {
+            if (!mp.is_reverse && mp.base == 'T'){
+                T_forward = true;
+            } else if (mp.is_reverse && mp.base == 'A')  {
+                A_reverse = true;
+            } else if (mp.base == 'C') {
+                C = true;
+            } else if (mp.base == 'G') {
+                G = true;
+            }
+        }
+
+        // Determine damage type
+        if (C && T_forward) {
+            return DamageType.CT;
+        } else if (G && A_reverse) {
+            return DamageType.GA;
+        } else {
+            return DamageType.NONE;
+        }
+    }
+}
