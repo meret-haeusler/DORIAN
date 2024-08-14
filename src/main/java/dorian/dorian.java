946efb37bbb0a0c2d0283ec7882e10e90100a1d2
meret <meret.haeusler@uni-tuebingen.de>
2024-08-14 14:15:06 +0200

    [UPDATE] Simplify main programme

--- src/main/java/dorian/dorian.java
+++ src/main/java/dorian/dorian.java
@@ -1,23 +1,24 @@
 package dorian;
 
 import cli.CLIParser;
-import datastructure.*;
+import datastructure.CorrectionMode;
+import datastructure.Fasta;
+import datastructure.ReturnTuple;
 import htsjdk.variant.variantcontext.VariantContext;
-import htsjdk.variant.vcf.*;
+import htsjdk.variant.vcf.VCFHeader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import me.tongfei.progressbar.*;
+import utils.FastaIO;
+import utils.VCFFileWriter;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.Date;
+import java.util.List;
 
-import static dorian.BaseCalling.makeBaseCall;
+import static dorian.BaseCalling.consensusCalling;
 
 /**
  * @author Meret Häusler
@@ -31,9 +32,8 @@ public class dorian {
     public static CorrectionMode cor_mode;
     public static List<Double> dp5;
     public static List<Double> dp3;
-    public static int cov;
     public static double freq;
-    public static Character base_call;
+    public static int cov;
 
     public static void main(String[] args) throws Exception {
 
@@ -52,10 +52,8 @@ public class dorian {
         // Correction mode
         cor_mode = cli_parser.COR_MODE;
         // Damage profiles
-        if (cor_mode.needsDP()) {
-            dp5 = cli_parser.DP5;
-            dp3 = cli_parser.DP3;
-        }
+        dp5 = cor_mode.needsDP() ? cli_parser.DP5 : dp5;
+        dp3 = cor_mode.needsDP() ? cli_parser.DP3 : dp3;
         // Output directory
         Path out_path = cli_parser.OUT;
         // Minimal Coverage
@@ -68,10 +66,10 @@ public class dorian {
         String sample_name = cli_parser.SAMPLE_NAME;
 
 
-        // MAIN PROGRAMME //
+        // PREPARE LOG FILES //
         if (cor_mode.equals(CorrectionMode.NO_COR)) {
             file_logger.info("\nCalls:");
-            file_logger.info("POS\tBASE_CALL\tBASE_FREQ");
+            file_logger.info("CHROM\tPOS\tREF\tCOV\tALLELE_COUNTS\tBASE_CALL\tBASE_FREQ");
         }
         if (!cor_mode.equals(CorrectionMode.NO_COR)) {
             file_logger.info("\nCorrected variants:");
@@ -84,28 +82,17 @@ public class dorian {
             roi_tab.info("#CHROM\tROI_START\tROI_END\tCORRECTED_POS");
         }
 
-        // Initialise Progress bar
-        StringBuilder consensus_sequence = null;
-        List<VariantContext> variant_calls = List.of();
-        try (ProgressBar pb = new ProgressBarBuilder()
-                .setInitialMax(ref.getSequence().length())
-                .setStyle(ProgressBarStyle.ASCII)
-                .setTaskName("[" + time_stamp + "] INFO Variant Calling:")
-                .setMaxRenderedLength(120)
-                .setUpdateIntervalMillis(100)
-                .build()) {
 
-            //TODO: Add BaseCalling call and resolve returns
-            ReturnTuple BaseCallingOut = makeBaseCall(reads, cov, freq, ref, "dummy");
-            consensus_sequence = BaseCallingOut.getSeq();
-            variant_calls = BaseCallingOut.getVariants();
+        // ADD STATUS BAR //
+        // Start the updating message in a separate thread
+        Thread updatingMessage = getUpdatingMessage();
 
-            pb.step();
 
-        } catch (Exception e){
-            logger.error("Variant Calling ran into an issue: " + e.getMessage());
-            System.exit(-1);
-        }
+        // MAIN PROGRAMME //
+        //Add BaseCalling call and resolve returns
+        ReturnTuple BaseCallingOut = consensusCalling(reads, cov, freq, ref, sample_name);
+        StringBuilder consensus_sequence = BaseCallingOut.getSeq();
+        List<VariantContext> variant_calls = BaseCallingOut.getVariants();
 
 
         // OUTPUT //
@@ -116,94 +103,58 @@ public class dorian {
 
         // Define vcf output
         String vcf_out = out_path + "/" + sample_name + "_" + cor_mode.getShortName() + ".vcf";
-        VCFHeader vcf_header = defaultHeader(ref, sample_name+ "_" + cor_mode.getShortName());
-        VCFEncoder vcf_writer = new VCFEncoder(vcf_header, true, true);
-        try (FileWriter writer = new FileWriter(vcf_out, false)) {
-            BufferedWriter bw = new BufferedWriter(writer);
-
-            // Write VCF header to the file
-            for (VCFHeaderLine headerline : vcf_header.getMetaDataInSortedOrder()) {
-                bw.write("##" + headerline);
-                bw.newLine();
-            }
-
-            // Write column identifiers
-            writeColumnHeader(bw, vcf_header);
-
-            // Write each VariantContext to the VCF file
-            for (VariantContext variant : variant_calls) {
-                vcf_writer.write(bw, variant);
-                bw.newLine();
-            }
-
-            bw.close();
-
-        } catch (Exception e) {
-            logger.error(e.getMessage());
-        }
+        VCFHeader vcf_header = VCFFileWriter.defaultHeader(ref, sample_name + "_" + cor_mode.getShortName());
+        VCFFileWriter.writeVCFFile(vcf_out, vcf_header, variant_calls);
 
 
         // OUTPUT INFO //
-        // Move log and roi file to output directory
+        // Move log file and ROI to output directory
         Files.move(Path.of("file.log"),
                 Path.of(out_path + "/" + time_stamp + "_" + sample_name + "_" + cor_mode.getShortName() + ".log"));
+        if (!cor_mode.equals(CorrectionMode.NO_COR)) {
+            Files.move(Path.of("roi.bed"),
+                    Path.of(out_path + "/" + time_stamp + "_" + sample_name + "_" + cor_mode.getShortName() + ".bed"));
+        }
+
+        // Update status bar
+        updatingMessage.interrupt();
+        System.out.println("\rDORIAN completed successfully.\n");
 
         // Output result paths
-        System.out.println();
         logger.info("Result files:");
         logger.info("Corrected variants written to: " + vcf_out);
         logger.info("Reconstructed genome written to: " + fasta_path);
         logger.info("Log file written to: " + out_path + "/" + time_stamp + "_" + sample_name + "_" + cor_mode.getShortName() + ".log");
-        if (!cor_mode.equals(CorrectionMode.NO_COR)){
-            Files.move(Path.of("roi.bed"),
-                    Path.of(out_path + "/" + time_stamp + "_" + sample_name + "_" + cor_mode.getShortName() + ".bed"));
+
+        // If exists, print ROI file path
+        if (!cor_mode.equals(CorrectionMode.NO_COR)) {
             logger.info("ROI table (IGV format) for corrected variants written to: " +
                     out_path + "/" + time_stamp + "_" + sample_name + "_" + cor_mode.getShortName() + ".bed");
         }
-        System.out.println();
-        logger.info("DORIAN completed successfully.");
     }
 
     /**
-     * Writes the VCF column header to the specified output file
-     *
-     * @param bw        Writer for current file
-     * @param vcfHeader VCF header of output data
-     * @throws IOException Throws exception if fails
+     * @return Update message while main programme runs
      */
-    private static void writeColumnHeader(BufferedWriter bw, VCFHeader vcfHeader) throws IOException {
-        bw.write("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT");
-        for (String sample : vcfHeader.getSampleNamesInOrder()) {
-            bw.write("\t" + sample);
-        }
-        bw.newLine();
-    }
+    private static Thread getUpdatingMessage() {
+        Thread updatingMessage = new Thread(() -> {
+            String[] messages = {"Running", "Running.", "Running..", "Running..."};
+            int i = 0;
 
-    private static VCFHeader defaultHeader(Fasta ref, String sample_name){
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    // Print the message and overwrite it on the same line
+                    System.out.print("\r" + messages[i % messages.length]);
+                    i++;
+                    Thread.sleep(500);  // Update every 500 milliseconds
+                } catch (InterruptedException e) {
+                    // Stop the updating message if interrupted
+                    break;
+                }
+            }
+        });
 
-        VCFHeaderVersion version = VCFHeaderVersion.valueOf("VCF4_2");
-
-        Map<String, String> contig_map =  Map.of(
-                "ID", ref.getHeader().split(" ")[0].replace(">", ""),
-                "length", Integer.toString(ref.getSequence().length()));
-        VCFHeaderLine contig = new VCFContigHeaderLine(contig_map,0);
-        VCFHeaderLine ad = new VCFFormatHeaderLine("AD", VCFHeaderLineCount.R, VCFHeaderLineType.Integer,
-                "Allelic depths for the ref and alt alleles in the order listed");
-        VCFHeaderLine dp = new VCFFormatHeaderLine("DP", 1, VCFHeaderLineType.Integer,
-                "Approximate read depth (reads with MQ=255 or with bad mates are filtered)");
-        VCFHeaderLine cor_mode_filter = new VCFInfoHeaderLine("COR_MODE", 1, VCFHeaderLineType.String,
-                "Used correction mode: " + dorian.cor_mode.getModeName());
-        VCFHeaderLine cov_1_filter = new VCFInfoHeaderLine("MIN_COV_1", 2, VCFHeaderLineType.String,
-                "Minimal coverage filter (incl. N): " + dorian.cov);
-        VCFHeaderLine freq_filter = new VCFInfoHeaderLine("MIN_FREQ", 3, VCFHeaderLineType.String,
-                "Minimal base frequency filter: " + dorian.freq);
-
-        Set<VCFHeaderLine> meta_data = new HashSet<>(Arrays.asList(contig, ad, dp, cor_mode_filter, cov_1_filter, freq_filter));
-
-        VCFHeader header = new VCFHeader(meta_data, new ArrayList<>(Collections.singleton(sample_name)));
-
-        header.setVCFHeaderVersion(version);
-
-        return header;
+        updatingMessage.start();
+        return updatingMessage;
     }
 }
