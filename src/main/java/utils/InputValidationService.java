6400612e59362fa42bad130adb836eb1daa38ac4
meret <meret.haeusler@uni-tuebingen.de>
2025-10-22 13:43:07 +0200

    [BUGFIX] Indexes the bam file if no index file is found (#2)

--- src/main/java/utils/InputValidationService.java
+++ src/main/java/utils/InputValidationService.java
@@ -1,7 +1,10 @@
 package utils;
 
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.io.FilenameUtils;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,9 +13,30 @@ import static dorian.dorian.logger;
 
 public class InputValidationService {
     public static void validateInputs(CommandLine cmd) {
-        checkFile(cmd, "bam");
         checkFile(cmd, "reference");
 
+        // Check if BAM file and BAM index file exist; Create index if missing
+        checkFile(cmd, "bam");
+        if (!checkBamIndex(cmd.getOptionValue("bam"))) {
+            // If index file does not exist, create with samtools
+            logger.info("BAM index file not found. Creating index using samtools.");
+            try {
+                ProcessBuilder pb = new ProcessBuilder("samtools", "index", cmd.getOptionValue("bam"));
+                Process process = pb.start();
+                int exitCode = process.waitFor();
+                if (exitCode != 0) {
+                    logger.error("Failed to create BAM index file.");
+                    System.exit(1);
+                } else {
+                    logger.info("BAM index file successfully created.\n");
+                }
+            } catch (Exception e) {
+                logger.error("Error while creating BAM index file: {}", e.getMessage());
+                System.exit(1);
+            }
+        }
+
+        // Check validity of correction mode and required damage profile files
         String correction = cmd.getOptionValue("correction");
         if (correction.equals("w")) {
             if (cmd.hasOption("dp3") && cmd.hasOption("dp5")) {
@@ -55,5 +79,27 @@ public class InputValidationService {
             logger.error("File not found: {}", value);
             System.exit(1);
         }
+    }
+
+    private static boolean checkBamIndex(String bamPath) {
+        String bamFilePath = FilenameUtils.getFullPath(bamPath);
+        String bamFileBasename = FilenameUtils.getBaseName(bamPath);
+
+        Path dir = Paths.get(bamFilePath);
+        String pattern = bamFileBasename + "*.bai"; // e.g., sample*.bai
+
+        // --- Check if any .bai file exists matching the prefix ---
+        boolean indexExists = false;
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, pattern)) {
+            for (Path entry : stream) {
+                if (Files.isRegularFile(entry)) {
+                    indexExists = true;
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            logger.error("Error reading directory {}: {}", bamFilePath, e.getMessage());
+        }
+        return indexExists;
     }
 }
